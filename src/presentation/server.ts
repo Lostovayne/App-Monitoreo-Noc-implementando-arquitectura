8fe1f5dcb32872d52835fa72ae3e56f81e1a5493
LostoVayne <moises.barrera.dev@gmail.com>
2023-10-31 04:05:08 -0300

    arreglo de LogRepository

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
@@ -9,12 +10,13 @@ import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
 //* se está usando como inyector de dependencia para el uso del file system
-const logRepository = new LogRepositoryImpl(
-    // new FileSystemDataSource()
-    // new MongoLogDatasource()
-    new PostgresLogDatasource()
-);
+const logRepository = new LogRepositoryImpl(new PostgresLogDatasource());
 const emailService = new EmailService();
+
+//* arreglo de logRepository
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const postgresLogRepository = new LogRepositoryImpl(new PostgresLogDatasource());
 
 export class Server {
     public static start() {
@@ -34,10 +36,20 @@ export class Server {
         // EmailServiceWithFileSystem.sendEmailWithFileSystemLogs(["epsaind@gmail.com"]);
 
         //* Crear una tarea de cron cada cierto tiempo
+        // CronService.createJob("*/5 * * * * *", () => {
+        //     const url = "https://google.com";
+        //     const checkService = new CheckService(
+        //         logRepository,
+        //         () => console.log(`${url} is ok`),
+        //         (error) => console.error(error)
+        //     );
+        //     checkService.execute(url);
+        // });
+
         CronService.createJob("*/5 * * * * *", () => {
             const url = "https://google.com";
-            const checkService = new CheckService(
-                logRepository,
+            const checkService = new CheckServiceMultiple(
+                [fsLogRepository, mongoLogRepository, postgresLogRepository],
                 () => console.log(`${url} is ok`),
                 (error) => console.error(error)
             );
